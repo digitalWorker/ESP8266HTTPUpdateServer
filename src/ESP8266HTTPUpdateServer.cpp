e203f69cd8836f549de511c2a8c7ded1de23eea2
digitalWorker <22681187+digitalWorker@users.noreply.github.com>
2017-12-13 09:59:37 +0100

    add bootstrap
    add basic bootstrap

--- src/ESP8266HTTPUpdateServer.cpp
+++ src/ESP8266HTTPUpdateServer.cpp
@@ -8,11 +8,90 @@
 
 
 static const char serverIndex[] PROGMEM =
-  R"(<html><body><form method='POST' action='' enctype='multipart/form-data'>
-                  <input type='file' name='update'>
-                  <input type='submit' value='Update'>
-               </form>
-         </body></html>)";
+  R"(<!doctype html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
+    
+
+    <title>asvin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' integrity='sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb' crossorigin='anonymous'>
+
+    <!-- Custom styles for this template -->
+    <link href='style.css' rel='stylesheet'>
+  </head>
+
+  <body>
+    <header>
+      <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+        <a class='navbar-brand' href='#'>Dashboard</a>
+        <button class='navbar-toggler d-lg-none' type='button' data-toggle='collapse' data-target='#navbarsExampleDefault' aria-controls='navbarsExampleDefault' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+
+        <div class='collapse navbar-collapse' id='navbarsExampleDefault'>
+          <ul class='navbar-nav mr-auto'>
+            <li class='nav-item active'>
+              <a class='nav-link' href='#'>Home <span class='sr-only'>(current)</span></a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>Settings</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>Profile</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>Help</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <div class='container-fluid' style='padding-top:60px;'>
+      <div class='row'>
+        <nav class='col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar'>
+          <ul class='nav nav-pills flex-column'>
+            <li class='nav-item'>
+              <a class='nav-link active' href='#'>Overview <span class='sr-only'>(current)</span></a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>Lorem</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>Ipsum</a>
+            </li>
+          </ul>
+        </nav>
+
+        <main role='main' class='col-sm-9 ml-sm-auto col-md-10 pt-3'>
+          <h1>Dashboard</h1>
+
+          <h2>Section title</h2>
+          <div>
+            <form class='form-inline mt-2 mt-md-0' method='POST' action='' enctype='multipart/form-data'>
+                <input class='form-control mr-sm-2' type='file' name='update'>
+                <button class='btn btn-outline-success my-2 my-sm-0' type='submit' value='Update'>Update</button>
+            </form>
+          </div>
+          
+        </main>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js' integrity='sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh' crossorigin='anonymous'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js' integrity='sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ' crossorigin='anonymous'></script>
+  </body>
+</html>)";
 static const char successResponse[] PROGMEM = 
   "<META http-equiv=\"refresh\" content=\"15;URL=/\">Update Success! Rebooting...\n";
 
